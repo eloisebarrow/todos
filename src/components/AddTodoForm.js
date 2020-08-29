e6d3768d529737ee066ad4ac88283e26e46e7c53
Eloise Barrow <eloise@chartbeat.com>
2020-08-29 12:29:14 -0400

    add todo and form component, refactor App

---
+++ src/components/AddTodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AddTodoForm() {
+  return (
+    <div>
+      <form>
+        <input placeholder="Add new todo" />
+        <button>Add</button>
+      </form>
+    </div>
+  )
+}
