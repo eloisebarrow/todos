6d9a514013ddab02f7cbfded2f8bc0d7b5a1c9c4
Eloise Barrow <eloise@chartbeat.com>
2020-08-29 12:47:07 -0400

    successfully handling form change

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import AddTodoForm from './components/AddTodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
-
+  // HOOKS
   const [newTodo, setNewTodo] = useState('')
   const [todoStatus, setTodoStatus] = useState('Incomplete')
+  const [todoList, setTodoList] = useState([])
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setNewTodo(e.target.value)
+  }
 
   return (
     <div className="App">
       <h1>To Dos</h1>
       <AddTodoForm 
         newTodo={newTodo} 
-        setNewTodo={setNewTodo} />
-      <TodoList />
+        setNewTodo={setNewTodo}
+        handleChange={handleChange} />
+      <TodoList 
+        newTodo={newTodo}
+        todoStatus={todoStatus}
+        setTodoStatus={setTodoStatus} />
     </div>
   );
 }
--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export default function AddTodoForm({ newTodo }) {
+export default function AddTodoForm({ newTodo, setNewTodo, handleChange }) {
   return (
     <div>
       <form>
         <input 
           placeholder="Add new todo"
           value={newTodo}
+          onChange={(e) => handleChange(e)}
         />
         <button>Add</button>
       </form>
